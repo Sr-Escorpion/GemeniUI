7d826e31927af4c440b08b773bf06e177372e9c6
Sr-Escorpion <atroncoso030@gmail.com>
2024-08-14 15:27:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ safety_settings = [
 ]
 
 # Model name
-MODEL_NAME = "gemini-1.5-pro-exp-0801"
-# MODEL_NAME = "gemini-1.5-pro-latest"
+# MODEL_NAME = "gemini-1.5-pro-exp-0801"
+MODEL_NAME = "gemini-1.5-pro-latest"
 
 # Framework selection (e.g., Tailwind, Bootstrap, etc.)
 framework = "Regular CSS use flex grid etc"  # Change this to "Bootstrap" or any other framework as needed
